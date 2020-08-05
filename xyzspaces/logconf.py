b87b6cf4d68d47a5647cc71f1d97d682b6b03641
Kharude, Sachin <sachin.kharude@here.com>
2020-08-05 12:49:16 +0530

    fixed linting error
    Signed-off-by: Kharude, Sachin <sachin.kharude@here.com>

--- xyzspaces/logconf.py
+++ xyzspaces/logconf.py
@@ -53,6 +53,7 @@ def setup_logging(
 # Set up logging to ``/dev/null`` like a library is supposed to.
 # http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
 
+
 class NullHandler(logging.Handler):
     """NullHandler class is a  'no-op' handler for use by library developers."""
 
